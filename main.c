78843336982354bfe5ee5ae52afffc447a327d1d
ShineChang <37838067+ShineChang@users.noreply.github.com>
2018-09-07 17:34:02 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,73 +2,57 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	float fp[2][4], sort[2][4];
-	int num, i, j, k;
-	float x, y;
+void swap(int *a, int *b)
+{
+  int tmp;
+  tmp = *a;
+  *a = *b;
+  *b = tmp;                                                                                             
+}
+int main(void) {
+	int num, i, j, k;;
 	scanf("%d", &num);
-	for(i = 1 ; i <= num ; i++)
+	for(i = 1 ; i < num+1 ; i++)
 	{
+		int fp[2][4], angle[4], length[4];
+		
 		for(j = 0 ; j < 4 ; j++)
 		{
 			for(k = 0 ; k < 2 ; k++)
-				scanf("%f", &fp[k][j]);
+				scanf("%d", &fp[k][j]);
 		}
-		x = (fp[0][0] + fp[0][1] + fp[0][2] + fp[0][3])/4;
-		y = (fp[1][0] + fp[1][1] + fp[1][2] + fp[1][3])/4;
-		for(j = 0 ; j <= 3 ; j++)
-			fp[0][j] -= x;
-			fp[1][j] -= y;
-		for(j = 0 ; j <= 3 ; j++)
+		if(((fp[0][1] - fp[0][0])*(fp[1][2] - fp[1][0]) - (fp[1][1] - fp[1][0])*(fp[0][2] - fp[0][1])) > 0)
 		{
-			if(fp[0][j] < 0 && fp[1][j] > 0){
-			sort[0][0] = fp[0][j];
-			sort[1][0] = fp[1][j];
-			}	
-			else if(fp[0][j] > 0 && fp[1][j] > 0){
-			sort[0][1] = fp[0][j];
-			sort[1][1] = fp[1][j];
-			}
-			else if(fp[0][j] > 0 && fp[1][j] < 0){
-			sort[0][2] = fp[0][j];
-			sort[1][2] = fp[1][j];
-			}
-			else if(fp[0][j] < 0 && fp[1][j] < 0){
-			sort[0][3] = fp[0][j];
-			sort[1][3] = fp[1][j];
-			}
-			
-			else if(fp[0][j] < 0 && fp[1][j] == 0){
-			sort[0][0] = fp[0][j];
-			sort[1][0] = fp[1][j];
-			}
-			else if(fp[0][j] == 0 && fp[1][j] > 0){
-			sort[0][1] = fp[0][j];
-			sort[1][1] = fp[1][j];
-			}
-			else if(fp[0][j] > 0 && fp[1][j] == 0){
-			sort[0][2] = fp[0][j];
-			sort[1][2] = fp[1][j];
-			}
-			else if(fp[0][j] == 0 && fp[1][j] < 0){
-			sort[0][3] = fp[0][j];
-			sort[1][3] = fp[1][j];
-			}
+			swap(&fp[0][1], &fp[0][2]);
+			swap(&fp[1][1], &fp[1][2]);
 		}
-		int angle[4], length[4];
-		angle[0] = (sort[0][1] - sort[0][0])*(sort[0][2] - sort[0][1]) + (sort[1][1] - sort[1][0])*(sort[1][2] - sort[1][1]);
-		angle[1] = (sort[0][1] - sort[0][0])*(sort[0][3] - sort[0][0]) + (sort[1][1] - sort[1][0])*(sort[1][3] - sort[1][0]);
-		angle[2] = (sort[0][2] - sort[0][3])*(sort[0][3] - sort[0][0]) + (sort[1][2] - sort[1][3])*(sort[1][3] - sort[1][0]);
-		angle[3] = (sort[0][2] - sort[0][3])*(sort[0][2] - sort[0][1]) + (sort[1][2] - sort[1][3])*(sort[1][2] - sort[1][1]);
-		length[0] = pow((sort[0][1] - sort[0][0]) , 2) + pow((sort[1][1] - sort[1][0]) , 2);
-		length[1] = pow((sort[0][2] - sort[0][1]) , 2) + pow((sort[1][2] - sort[1][1]) , 2);
-		length[2] = pow((sort[0][2] - sort[0][3]) , 2) + pow((sort[1][2] - sort[1][3]) , 2);
-		length[3] = pow((sort[0][3] - sort[0][0]) , 2) + pow((sort[1][3] - sort[1][0]) , 2);
-		printf("%1.2f\n%1.2f", x, y);
+		if(((fp[0][1] - fp[0][0])*(fp[1][3] - fp[1][0]) - (fp[1][1] - fp[1][0])*(fp[0][3] - fp[0][1])) > 0)
+		{
+			swap(&fp[0][1], &fp[0][3]);
+			swap(&fp[1][1], &fp[1][3]);
+		}
+		if(((fp[0][2] - fp[0][0])*(fp[1][3] - fp[1][0]) - (fp[1][2] - fp[1][0])*(fp[0][3] - fp[0][1])) > 0)
+		{
+			swap(&fp[0][3], &fp[0][2]);
+			swap(&fp[1][3], &fp[1][2]);
+		}
+		for(j = 0 ; j < 4 ; j++)
+		{
+			for(k = 0 ; k < 2 ; k++)
+				printf("%d", fp[k][j]);
+		}
+		angle[0] = (fp[0][1] - fp[0][0])*(fp[0][2] - fp[0][1]) + (fp[1][1] - fp[1][0])*(fp[1][2] - fp[1][1]);
+		angle[1] = (fp[0][1] - fp[0][0])*(fp[0][3] - fp[0][0]) + (fp[1][1] - fp[1][0])*(fp[1][3] - fp[1][0]);
+		angle[2] = (fp[0][2] - fp[0][3])*(fp[0][3] - fp[0][0]) + (fp[1][2] - fp[1][3])*(fp[1][3] - fp[1][0]);
+		angle[3] = (fp[0][2] - fp[0][3])*(fp[0][2] - fp[0][1]) + (fp[1][2] - fp[1][3])*(fp[1][2] - fp[1][1]);
+		length[0] = pow((fp[0][1] - fp[0][0]) , 2) + pow((fp[1][1] - fp[1][0]) , 2);
+		length[1] = pow((fp[0][2] - fp[0][1]) , 2) + pow((fp[1][2] - fp[1][1]) , 2);
+		length[2] = pow((fp[0][2] - fp[0][3]) , 2) + pow((fp[1][2] - fp[1][3]) , 2);
+		length[3] = pow((fp[0][3] - fp[0][0]) , 2) + pow((fp[1][3] - fp[1][0]) , 2);
+		
 		if(angle[0] == 0 && angle[1] == 0 && angle[2] == 0 && angle[3] == 0 && length[0] == length[1] && length[2] == length[3] && length[0] == length[3])
 			printf("square");
-		else if(angle[0] == 0 && angle[1] == 0 && angle[2] == 0 && angle[3] == 0 && length[0] == length[2] && length[1] == length[3])
+		else if(angle[0] == 0 && angle[1] == 0 && angle[2] == 0 && angle[3] == 0)
 			printf("rectangle");
 		else if(length[0] == length[1] && length[2] == length[3] && length[0] == length[3])
 			printf("diamond");
